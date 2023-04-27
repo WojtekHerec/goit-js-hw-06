62d81296cc5509d7615b437cbf691d8d1435c729
WojtekHerec <wojtekherec@wp.pl>
2023-04-27 17:42:17 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,8 @@
-// Znajdujemy listę kategorii na podstawie id
-const categoriesList = document.querySelector("#categories");
+const arrayCategories = document.querySelectorAll(".item");
 
-// Znajdujemy wszystkie elementy li.item w liście kategorii
-const categoryItems = categoriesList.querySelectorAll("li.item");
+console.log(`Number of categories: ${arrayCategories.length}`);
 
-// Wypisujemy w konsoli liczbę kategorii
-console.log(`Number of categories: ${categoryItems.length}`);
-
-// Dla każdego elementu li.item znajdujemy i wypisujemy w konsoli tekst nagłówka oraz liczbę elementów w danej kategorii
-categoryItems.forEach((categoryItem) => {
-  // Znajdujemy tekst nagłówka wewnątrz elementu li.item
-  const categoryTitle = categoryItem.querySelector("h2").textContent;
-
-  // Znajdujemy wszystkie elementy li w obrębie elementu li.item
-  const categoryElements = categoryItem.querySelectorAll("li");
-
-  // Wypisujemy w konsoli nazwę kategorii oraz liczbę elementów
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${categoryElements.length}`);
+arrayCategories.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
